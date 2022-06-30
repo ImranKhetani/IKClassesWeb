02da4a2743c1a52fc27847ce3561956a1e7f9c24
Imran Khetani <imr@sitecore.net>
2022-06-30 10:26:40 +0800

    Database Added

---
+++ IKClassesWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IKClassesWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IKClassesWebApp.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
